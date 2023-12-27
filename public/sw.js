b8b0744f8253674dc504ec78cd0a6d76fac6f37b
mohdasaadeh <mass.1993@hotmail.com>
2023-12-27 10:24:10 +0300

    handling static caching

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,26 @@
 self.addEventListener("install", (event) => {
   console.log("The service worker has been installed successfully!", event);
+
+  event.waitUntil(
+    caches.open("static").then((cache) => {
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/help",
+        "/help/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/css/help.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
@@ -7,5 +28,13 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
 });

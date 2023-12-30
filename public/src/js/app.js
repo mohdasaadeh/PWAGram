409b98846a6fdc0ae4ecf6305f2cbc2209619fd6
mohdasaadeh <mass.1993@hotmail.com>
2023-12-30 15:05:39 +0300

    configuring the notification to the service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,7 @@
+const enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then((event) => {
     console.log("The service worker has been registered successfully!", event);
@@ -15,3 +19,52 @@ window.addEventListener("beforeinstallprompt", (event) => {
 
   return false;
 });
+
+function displayNotification() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then((sw) => {
+      sw.showNotification("Notification permission granted!", {
+        body: "You successfully subscribed to our Notification service!",
+        icon: "/src/images/icons/app-icon-96x96.png",
+        image: "/src/images/sf-boat.jpg",
+        dir: "ltr",
+        lang: "en-US", // BCP 47,
+        vibrate: [100, 50, 200],
+        badge: "/src/images/icons/app-icon-96x96.png",
+        tag: "confirm-notification",
+        renotify: true,
+        actions: [
+          {
+            action: "confirm",
+            title: "Okay",
+            icon: "/src/images/icons/app-icon-96x96.png",
+          },
+          {
+            action: "cancel",
+            title: "Cancel",
+            icon: "/src/images/icons/app-icon-96x96.png",
+          },
+        ],
+      });
+    });
+  }
+}
+
+function askForNotificationsPermission() {
+  Notification.requestPermission().then((permission) => {
+    if (permission !== "granted") {
+      console.log("The notification permission was not granted");
+    } else {
+      displayNotification();
+    }
+  });
+}
+
+for (const button of enableNotificationsButtons) {
+  if ("Notification" in window) {
+    button.style.display = "block";
+    button.addEventListener("click", askForNotificationsPermission);
+  } else {
+    button.style.display = "hidden";
+  }
+}
